62b4315d70710cf47448ec978ad4bd953f57ae5e
Park9eon <quiet9.noise@gmail.com>
2018-09-17 15:45:14 +0900

    손잡이와 연결선 디자인 및 위치조정과 당기는 이벤트처리 완료

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ var HANDLE_STATUS_FALL = 1;
 // 핸들이 올라가는경우
 var HANDLE_STATUS_GOUP = 2;
 
+// 목표지점
+var HANDLE_X = null;
+var HANDLE_Y = null;
+
+var LINE_X = null;
+var LINE_Y = null;
+
 var app = null;
 var conch = null;
 var handle = null;
+var line = null;
 
-var handleWeight = 10;
-var lineHeight = 200;
 
 // 페이지 로딩완료
 window.onload = function () {
@@ -30,10 +36,13 @@ window.onload = function () {
     conch.anchor.set(0.5);
     app.stage.addChild(conch);
 
+    line = new PIXI.Graphics();
+    app.stage.addChild(line);
+
     // 핸들
     handle = new PIXI.Graphics();
-    handle.lineStyle(20, 0xccd6dd);
-    handle.beginFill(0x66757f, 1);
+    handle.lineStyle(20, 0xffffff);
+    handle.beginFill(0x66757f, 0);
     handle.drawCircle(0, 0, 30);
     handle.endFill();
     handle.buttonMode = true;
@@ -47,20 +56,26 @@ window.onload = function () {
     handle.status = HANDLE_STATUS_FIXED;
     app.stage.addChild(handle);
     onRender();
+    drawLine(handle.x, handle.y);
     // handle의 움직임
     app.ticker.add(function (delta) {
         // 핸들이 떨어지는경우
         if (handle.status === HANDLE_STATUS_FALL) {
-            if (handle.y > 700) {
-                // 핸들이 최대한 떨어짐
-                handle.status = HANDLE_STATUS_GOUP;
-            } else {
-                handle.y += delta * 10;
-            }
         }
         // 핸들이 올라가는 경우
         if (handle.status === HANDLE_STATUS_GOUP) {
-            handle.y -= delta * 3;
+            var height = handle.y - HANDLE_Y;
+            var width = handle.x - HANDLE_X;
+            var scale = Math.abs(height / width);
+            var speed = 10 * Math.abs(width) / Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+            handle.y += scale * speed * (height > 0 ? -1 : 1);
+            handle.x += speed * (width > 0 ? -1 : 1);
+            if (Math.abs(width) < speed && Math.abs(height < scale * speed)) {
+                handle.x = HANDLE_X;
+                handle.y = HANDLE_Y;
+                handle.status = HANDLE_STATUS_FIXED;
+            }
+            drawLine(handle.x, handle.y);
         }
         // 핸들이 한계까지 갔을경우
     });
@@ -83,6 +98,12 @@ function onRender() {
         conch.width = 512;
         conch.height = 512;
     }
+    HANDLE_X = app.screen.width / 2 + 90;
+    HANDLE_Y = app.screen.height / 2 - 180;
+    LINE_X = app.screen.width / 2 + 90;
+    LINE_Y = app.screen.height / 2 - 150;
+    handle.x = HANDLE_X;
+    handle.y = HANDLE_Y;
 }
 
 function onDragStart(event) {
@@ -94,7 +115,7 @@ function onDragStart(event) {
 function onDragEnd() {
     this.dragging = false;
     this.data = null;
-    handle.status = HANDLE_STATUS_FALL;
+    handle.status = HANDLE_STATUS_GOUP;
 }
 
 function onDragMove() {
@@ -102,5 +123,22 @@ function onDragMove() {
         var newPosition = this.data.getLocalPosition(this.parent);
         this.x = newPosition.x;
         this.y = newPosition.y;
+        drawLine(this.x, this.y);
     }
+}
+
+function drawLine(x, y) {
+    line.clear();
+    line.lineStyle(6, 0xffffff);
+    line.moveTo(LINE_X, LINE_Y);
+    var width = (LINE_X - x);
+    var height = (LINE_Y - y);
+    var scale = Math.abs(height / width);
+    var moveX = Math.sqrt(400 / (Math.pow(scale, 2) + 1)) * (width < 0 ? 1 : -1);
+    var moveY = scale * Math.abs(moveX) * (height < 0 ? 1 : -1);
+    line.lineTo(x - moveX, y - moveY);
+}
+
+function playRandomSound() {
+    
 }
